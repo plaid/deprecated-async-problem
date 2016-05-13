640114b3a3adebd29d2885ac5a6d68e22e2bad85
slavaGanzin <slava.ganzin@gmail.com>
2016-05-13 21:45:25 +0300

    main and fixes

--- async-seq.js
+++ async-seq.js
@@ -1,10 +1,11 @@
-const fs    = require('fs');
-const path  = require('path');
+const fs = require('fs');
+const path = require('path');
 const async = require('async');
 const R = require('ramda');
+const S = require('sanctuary');
 
 const join = (x, cb) => cb(null, path.join(process.argv[2], x));
-const map = R.curry(R.flip(async.map));
+const map = R.flip(async.map);
 
 const output = (err, results) => {
   if (err) {
@@ -16,11 +17,15 @@ const output = (err, results) => {
   }
 }
 
-async.seq(
-  join,
-  fs.readFile,
-  (x, cb) => cb(null, x.toString().replace(/\n$/,'').split('\n')),
-  map(join),
-  map(fs.readFile),
-  (files, cb) => cb(null, files.join(''))
-)('index.txt', (output))
+const main = () => {
+  async.seq(
+    join,
+    fs.readFile,
+    (x, cb) => cb(null, S.lines(x.toString())),
+    map(join),
+    map(fs.readFile),
+    (files, cb) => cb(null, files.join(''))
+  )('index.txt', output)
+}
+
+if (process.mainModule.filename === __filename) main();
